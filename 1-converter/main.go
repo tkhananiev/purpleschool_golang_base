bc44e34df68ac5b6056b25521b147fdaddb280ee
Timur Khananiev <timur712@gmail.com>
2025-10-17 07:39:43 +0300

    из EUR в RUB

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -4,6 +4,16 @@ import (
 	"fmt"
 )
 
+const (
+	usdToEur = 0.85
+	usdToRub = 80.5
+)
+
 func main() {
-	fmt.Println("First Project \"Converter\"")
+	fmt.Println(eurToRub(50))
+
+}
+
+func eurToRub(usd float64) float64 {
+	return usdToRub / usdToEur
 }
