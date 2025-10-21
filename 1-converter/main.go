f381ae0b8cc5fe0780ba4dfe87c24572c3953840
Timur Khananiev <timur712@gmail.com>
2025-10-21 14:05:53 +0300

    userInput function with input checks realized

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 const (
@@ -11,13 +12,64 @@ const (
 )
 
 func main() {
-	fmt.Println(eurToRub)
+
+	originalCurrency, quantity, finalCurrency := userInput()
+	fmt.Println(originalCurrency, quantity, finalCurrency)
 }
 
-func userInput(originalCurrency, intermediateCurrency, finalCurrency string) (string, string, string) {
-	return originalCurrency, intermediateCurrency, finalCurrency
-}
-
-func convert(quantity float64, originalCurrency string, finalCurrency string) float64 {
+func convert(originalCurrency string, quantity float64, finalCurrency string) float64 {
 	return 0
+}
+
+func userInput() (string, float64, string) {
+	var originalCurrency string
+	var finalCurrency string
+	var quantity float64
+	for {
+		fmt.Print("Введите исходную валюту - EUR/USD/RUB: ")
+		fmt.Scan(&originalCurrency)
+		originalCurrencyLower := strings.ToLower(originalCurrency)
+		if originalCurrencyLower != "eur" && originalCurrencyLower != "usd" && originalCurrencyLower != "rub" {
+			fmt.Println("Некорректный ввод.")
+			continue
+		} else {
+			originalCurrency = originalCurrencyLower
+			break
+		}
+	}
+	for {
+		fmt.Print("Введите количество: ")
+		fmt.Scan(&quantity)
+		quantityType := fmt.Sprintf("%T", quantity)
+		if quantityType != "float64" && quantityType != "int" {
+			fmt.Println("Некорректный ввод.")
+			continue
+		} else {
+			break
+		}
+	}
+	for {
+		switch originalCurrency {
+		case "eur":
+			fmt.Print("Введите целеву валюту - USD/RUB: ")
+		case "usd":
+			fmt.Print("Введите целеву валюту - EUR/RUB: ")
+		case "rub":
+			fmt.Print("Введите целеву валюту - EUR/USD: ")
+		}
+		fmt.Scan(&finalCurrency)
+		finalCurrencyLower := strings.ToLower(finalCurrency)
+		if finalCurrencyLower != "eur" && finalCurrencyLower != "usd" && finalCurrencyLower != "rub" {
+			fmt.Println("Некорректный ввод.")
+			continue
+		} else if finalCurrencyLower == originalCurrency {
+			fmt.Println("Целевая валюта не может совпадать с исходной.")
+			continue
+
+		} else {
+			finalCurrency = finalCurrencyLower
+			break
+		}
+	}
+	return originalCurrency, quantity, finalCurrency
 }
